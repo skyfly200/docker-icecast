64b49965b997c034ad58cb0fef7a5813337e31f5
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2019-05-21 12:24:19 -0600

    Add more ENV VAR parsing
    Add lines to parse new ENV VARs
    All ENV VARs:
    ICECAST_PORT - Port of the main listen-socket
    ICECAST_ADMIN - Email of server webmaster
    ICECAST_LOCATION - Public location of the server
    ICECAST_BURST - provide a burst of data when a client first connects. reduces startup time, but significantly increases latency
    ICECAST_BURST_SIZE - how many bytes to burst on connect
    ICECAST_HOSTNAME - Host of the server. used to build links and in the stream directory, if enabled
    ICECAST_MASTER_SERVER - Master relay source hostname or IP
    ICECAST_MASTER_SERVER_PORT - Master relay source port
    ICECAST_MATER_PASSWORD - Master relay source password
    ICECAST_RELAY_ON_DEMAND - All relays on-demand unless overridden
    ICECAST_CLIENT_LIMIT - Max clients that may connect to this server
    ICECAST_SOURCE_LIMIT - Max sources that may connect to this server
    ICECAST_QUE_SIZE - Size of the servers buffer queue

--- start.sh
+++ start.sh
@@ -20,6 +20,58 @@ if [ -n "$ICECAST_PASSWORD" ]; then
     sed -i "s/<password>[^<]*<\/password>/<password>$ICECAST_PASSWORD<\/password>/g" /etc/icecast2/icecast.xml
 fi
 
+if [ -n "$ICECAST_PORT" ]; then
+    sed -i "s/<port>8000<\/port>/<port>$ICECAST_PORT<\/port>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_ADMIN" ]; then
+    sed -i "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN<\/admin>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_LOCATION" ]; then
+    sed -i "s/<location>[^<]*<\/location>/<location>$ICECAST_LOCATION<\/location>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_BURST" ]; then
+    sed -i "s/<burst-on-connect>[^<]*<\/burst-on-connect>/<burst-on-connect>$ICECAST_BURST<\/burst-on-connect>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_BURST_SIZE" ]; then
+    sed -i "s/<burst-size>[^<]*<\/burst-size>/<burst-size>$ICECAST_BURST_SIZE<\/burst-size>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_HOSTNAME" ]; then
+    sed -i "s/<hostname>[^<]*<\/hostname>/<hostname>$ICECAST_HOSTNAME<\/hostname>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_MASTER_SERVER" ]; then
+    sed -i "s/<master-server>[^<]*<\/master-server>/<master-server>$ICECAST_MASTER_SERVER<\/master-server>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_MASTER_SERVER_PORT" ]; then
+    sed -i "s/<master-server-port>[^<]*<\/master-server-port>/<master-server-port>$ICECAST_MASTER_SERVER_PORT<\/master-server-port>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_MATER_PASSWORD" ]; then
+    sed -i "s/<master-password>[^<]*<\/master-password>/<master-password>$ICECAST_MASTER_PASSWORD<\/master-password>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_RELAY_ON_DEMAND" ]; then
+    sed -i "s/<relays-on-demand>[^<]*<\relays-on-demand>/<relays-on-demand>$ICECAST_RELAY_ON_DEMAND<\/relays-on-demand>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_CLIENT_LIMIT" ]; then
+    sed -i "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_CLIENT_LIMIT<\/clients>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_SOURCE_LIMIT" ]; then
+    sed -i "s/<sources>[^<]*<\/sources>/<sources>$ICECAST_SOURCE_LIMIT<\/sources>/g" /etc/icecast2/icecast.xml
+fi
+
+if [ -n "$ICECAST_QUE_SIZE" ]; then
+    sed -i "s/<queue-size>[^<]*<\/queue-size>/<queue-size>$ICECAST_QUE_SIZE<\/queue-size>/g" /etc/icecast2/icecast.xml
+fi
+
 #cat /etc/icecast2/icecast.xml
 
 set -e
